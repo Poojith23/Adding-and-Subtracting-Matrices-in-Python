86d139def3d4a71368fcbc4d1a693848af84e6a8
Poojith23 <114901320+Poojith23@users.noreply.github.com>
2023-01-09 22:22:42 +0530

    Add files via upload

---
+++ Adding and Subtracting Matrices in Python.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# importing numpy as np
+import numpy as np
+  
+  
+# creating first matrix
+A = np.array([[1, 2], [3, 4]])
+  
+# creating second matrix
+B = np.array([[4, 5], [6, 7]])
+  
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
+  
+# adding two matrix
+print("Addition of two matrix")
+print(np.add(A, B))
+
+
+# In[ ]:
+
+
+
+
